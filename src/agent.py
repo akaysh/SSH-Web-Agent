52cd3690c1ee09416c090c64b03ff06c7664bba8
Kishor Bhat <kishorbhat@gmail.com>
2015-10-01 13:31:12 +0530

    Web agent basic structure

---
+++ src/agent.py
@@ -0,0 +1,40 @@
+import socket
+import threading
+import json
+
+
+class ClientThread(threading.Thread):
+
+    def __init__(self, ip, port, socket):
+        threading.Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.socket = socket
+        print "[+] New thread started for "+ip+":"+str(port)
+
+    def run(self):
+        print "Connection from: "+ip+":"+str(port)
+        data = self.socket.recv(2048).strip()
+        message = json.loads(data)
+        print message
+
+host = "0.0.0.0"
+port = 8008
+
+tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+tcpsock.bind((host, port))
+threads = []
+
+while True:
+    tcpsock.listen(4)
+    print "\nListening for incoming connections..."
+    (clientsock, (ip, port)) = tcpsock.accept()
+    newThread = ClientThread(ip, port, clientsock)
+    newThread.start()
+    threads.append(newThread)
+
+for t in threads:
+    t.join()
+
