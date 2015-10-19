3a461719bb5345ecf898b08f29f23498a4953a78
chinmaydd <chinmay1dd@gmail.com>
2015-10-19 20:44:19 +0530

    Adds authentication request message

--- src/server.py
+++ src/server.py
@@ -6,6 +6,66 @@ a = 0
 # DH prime
 p = 0
 
+class ClientThread(threading.Thread):
+
+    def __init__(self, ip, port, socket):
+        threading.Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.socket = socket
+        print "[+] New thread started for " + ip + ":" + str(port)
+
+    def run(self):
+    	print "Connection from: "+ self.ip + ":" + str(self.port)
+    	data = self.socket.recv(10240).strip()
+    	message = json.loads(data)
+    	authentication_request(message)
+
+def wait():
+    # Declare host and port
+    host = "0.0.0.0"
+    port = 8009
+
+    tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    tcpsock.bind((host, port))
+
+    # Threading for multiple clients
+    threads = []
+
+    while True:
+        tcpsock.listen(4)
+        print "\n[+] Listening for incoming connections..."
+        (clientsock, (ip, port)) = tcpsock.accept()
+        newThread = ClientThread(ip, port, clientsock)
+        newThread.start()
+        threads.append(newThread)
+
+    # Join all threads
+    for t in threads:
+        t.join()
+
+def send(message):
+	# Socket-based transfer of data
+	source_ip = '127.0.0.1'
+	tcp_port = 8008
+	request_packet = json.dumps(message)
+
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	except:
+		print 'Socket cannot be created.'
+		sys.exit()
+
+	try:
+		s.connect((source_ip, tcp_port))
+	except:
+		print "Host not up! Exiting..."
+		sys.exit()
+	s.send(request_packet)
+	s.close()
+	print "[+] Sent Data."
+
 # Diffie-Hellman key exchange parameters
 def diffie_hellman():
 	# dh is an object of type `DiffieHellman`
@@ -85,17 +145,63 @@ def compute_secret(p, B):
 	# Secret = (B**a)%p
 	return pow(B, a, p)
 
-def compute_shared_secret(method, referer, e, f, S):
-    data = method + referer + str(e) + str(f) + str(S)
+def generate_ciphertext(shared_secret, secret_key, initialization_vector, identifier):
+	# Format
+	# byte[4] random
+	# byte AUTH_REQUEST
+	# string identifier
+	# string SSH session identifier
+	# byte[n] padding
 
-    return SHA256.new(data).digest()
+    # Random number of 128 bit size
+    r = random.getrandbits(128)
 
-def compute_hash(S, shared_secret, identifier, referer):
-    data = str(S) + shared_secret + identifier + referer
+    # type of request to be made.
+    # AUTH_REQUEST 0x03
+    type_of = 0x03
 
-    return SHA256.new(data).digest()
+    # SSH Session identifier
+    # As per RFC4253, The exchange hash H from the first key exchange is additionally used as the session identifier, which is a unique identifier for this connection
+    SSH_session_identifier = shared_secret
 
-def authentication_request(message):	
+    # Generate string for encryption
+    plaintext = str(r) + str(type_of) + str(identifier) + str(SSH_session_identifier)
+
+    # Add padding to plaintext since block size of AES_CBC_256 mode is 16
+    plaintext = add_padding(plaintext)
+
+    # Create an AES object
+    aes = AES.new(secret_key, AES.MODE_CBC, initialization_vector)
+    
+    # Ciphertext
+    ciphertext = aes.encrypt(plaintext)
+
+    return base64.b64encode(ciphertext)
+ 
+def generate_AUTH_REQUEST_message_body(shared_secret, secret_key, initialization_vector):
+	# Format
+	# byte algorithm (AES_256_CBC 0x02)
+	# string identifier (session_identifier)
+	# string ciphertext (encrypted part of message body)
+
+	message_body = dict()
+
+    # Specifies the type of algorithm to be used for encryption
+    # AES_256_CBC 0x02
+	message_body['algorithm'] = 0x02
+
+    # Session identifier set by agent.
+    # ******** TDB *********************
+    # identifier = generate_identifier()
+	identifier = '1'
+	message_body['session_identifier'] = identifier
+
+    # Generate Ciphertext
+	message_body['ciphertext'] = generate_ciphertext(shared_secret, secret_key, initialization_vector, identifier)
+
+	return message_body
+
+def authentication_request(data):	
 	# Shared secret computation
 	method = 'POST'
 	referer = '127.0.0.1'
@@ -103,7 +209,7 @@ def authentication_request(message):
 	e = A
 	# Diffie Hellman public key of agent
 	# f refers to 'B'
-	f = message['f']
+	f = data['f']
 	# secret key computed via DH key exchange
 	S = compute_secret(p, f)
 
@@ -120,75 +226,26 @@ def authentication_request(message):
 	aes = AES.new(secret_key, AES.MODE_CBC, initialization_vector)
 
     # received ciphertext
-	ciphertext = base64.b64decode(message['E']['ciphertext'])
+	ciphertext = base64.b64decode(data['E']['ciphertext'])
 
     # Plaintext obtained through AES decryption
 	plaintext = aes.decrypt(ciphertext)
 
+	# Acknowledge decryption of ciphertext
 	print "[+] Received plaintext!"
 
-class ClientThread(threading.Thread):
+	# Generate a message with version number and id
+	auth_message = message()
 
-    def __init__(self, ip, port, socket):
-        threading.Thread.__init__(self)
-        self.ip = ip
-        self.port = port
-        self.socket = socket
-        print "[+] New thread started for " + ip + ":" + str(port)
+	# add message request type
+	auth_message = request(auth_message, 'PRIVATE')
 
-    def run(self):
-    	print "Connection from: "+ self.ip + ":" + str(self.port)
-    	data = self.socket.recv(10240).strip()
-    	message = json.loads(data)
-    	authentication_request(message)
+	# message_body
+	auth_message['message'] = generate_AUTH_REQUEST_message_body(shared_secret, secret_key, initialization_vector)
 
-def wait():
-    # Declare host and port
-    host = "0.0.0.0"
-    port = 8009
-
-    tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    tcpsock.bind((host, port))
-
-    # Threading for multiple clients
-    threads = []
-
-    while True:
-        tcpsock.listen(4)
-        print "\n[+] Listening for incoming connections..."
-        (clientsock, (ip, port)) = tcpsock.accept()
-        newThread = ClientThread(ip, port, clientsock)
-        newThread.start()
-        threads.append(newThread)
-
-    # Join all threads
-    for t in threads:
-        t.join()
-
-def send(message):
-	# Socket-based transfer of data
-	source_ip = '127.0.0.1'
-	tcp_port = 8008
-	request_packet = json.dumps(message)
-
-	try:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	except:
-		print 'Socket cannot be created.'
-		sys.exit()
-
-	try:
-		s.connect((source_ip, tcp_port))
-	except:
-		print "Host not up! Exiting..."
-		sys.exit()
-	s.send(request_packet)
-	s.close()
-	print "[+] Sent Data."
-
-	wait()
+	send(auth_message)
 
 if __name__ == "__main__":
 
 	send(session_request())
+	wait()
